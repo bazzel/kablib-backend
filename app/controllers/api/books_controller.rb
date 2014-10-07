5f3101f28342ad7c1970a6a6dd37859d874182b9
Patrick Baselier <patrick@kabisa.nl>
2014-10-07 19:19:41 +0200

    Update book (basic flow)

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -2,4 +2,19 @@ class Api::BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def update
+    book = Book.find(params[:id])
+
+    if book.update(book_params)
+      render json: book
+    else
+      render json: { errors: book.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :description, :author)
+  end
 end
