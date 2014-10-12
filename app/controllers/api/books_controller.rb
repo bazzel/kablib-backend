28429cece3a4070e09e81b5a2c3fab7aac2efb4a
Patrick Baselier <patrick@kabisa.nl>
2014-10-12 14:12:38 +0200

    Add new books

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -7,6 +7,17 @@ class Api::BooksController < ApplicationController
     render json: Book.find(params[:id])
   end
 
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+      render json: book
+    else
+      render json: { errors: book.errors }, status: :unprocessable_entity
+    end
+
+  end
+
   def update
     book = Book.find(params[:id])
 
